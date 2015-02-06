db484465e16156916cc948f3a095e1d947f58aea
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-02-06 09:22:21 -0500

    Lab

--- 1-recursion/Driver.java
+++ 1-recursion/Driver.java
@@ -1,3 +1,11 @@
-public class Driver{
+public class Driver {
+
+    public static void main(String[] args) {
+	
+	Recursion R = new Recursion();
+	int a = R.lens("");
+	System.out.println(a);
+
+    }
 
 }
