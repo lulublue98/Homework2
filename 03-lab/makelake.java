953df2b20bacc1fc746e2939e45162f02e0e8bf0
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-02-24 09:14:57 -0500

    get it

--- 03-lab/makelake.java
+++ 03-lab/makelake.java
@@ -4,27 +4,47 @@ import java.util.*;
 public class makelake {
 
     int[][] pasture;
+    int maxX, maxY;
+    int[] data;
 
     public makelake(int R, int C, int E, int N) {
-	maxX=C;
-	maxY=R;
-	pasture = new int[maxX][maxY];
-	try {
-	    Scanner sc = new Scanner(new File("lake.txt"));
-	    int j=0;
-	    while (sc.hasNext()) {
-		String line = sc.nextLine();
-		for (int i=0;i<maxX;i++) {
-		    board[i][j] = line.intAt(i);
-		}
-		j++;
+	maxX = C;
+	maxY = R;
+	pasture = new int[maxY][maxX];
+    }
+    
+    public String toString() {
+	String s = "";
+	for (int i=0;i<pasture.length;i++) {
+	    for (int j=0;j<pasture[i].length;j++) {
+		s = s + pasture[i][j] + " ";
 	    }
+	    s = s + "\n";
 	}
-	catch (Exception e){
+	return s;
+    }
+
+    private void setData( int[] d ) {
+	data = new int[d.length];
+	for (int i=0;i<data.length;i++) {
+	    data[i] = d[i];
+	}
+	int x = 0;
+	for (int j=0;j<pasture.length;j++) {
+	    for (int h=0;h<pasture[j].length;h++) {
+		pasture[j][h] = data[x];
+		x = x + 1;
+	    }
 	}
     }
 
     public static void main( String[] args ) {
-	makelake M = new makelake();
+	makelake M = new makelake(4, 6, 22, 2);
+	int[] data = { 28, 25, 20, 32, 34, 36,
+		       27, 25, 20, 20, 30, 34,
+		       24, 20, 20, 20, 20, 30,
+		       20, 20, 14, 14, 20, 20 };
+	M.setData(data);
+	System.out.println(M);
     }
 }
