45b56f06110122aceae38cf38ca91c111c370409
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-02-25 09:26:55 -0500

    sort

--- 04-Mergesort/Mergesort.java
+++ 04-Mergesort/Mergesort.java
@@ -1,14 +1,23 @@
 public class Mergesort {
 
-    public Arraylist<Integer> a, b;
+    public Arraylist<Integer> one, two;
     public Arraylist<Integer> merge;
 
-    public Mergesort( Arraylist<Integer> c, Arraylist<Integer> d ) {
-	a = c;
-	b = d;
+    public Mergesort( Arraylist<Integer> a, Arraylist<Integer> b ) {
+	one = a;
+	two = b;
     }
 
-    public Arraylist<Integer> mergesort() {
+    public Arraylist<Integer> mergesort( Arraylist<Integer> a, Arraylist<Integer> b ) {
+	while ( a.size() > 0 && b.size() > 0 ) {
+	    if ( a.get(0) > b.get(0) ) {
+		merge.add(a.get(0));
+	    } else if ( b.get(0) > a.get(0) ) { 
+		merge.add(b.get(0));
+	    } else {
+		merge.add(a.get(0));
+	    }
+	}
 	
     }
 
