5abd37a3368f077a57feded8f16b270f4756a0d1
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-02-25 08:57:33 -0500

    homework

---
+++ 04-Mergesort/Mergesort.java
@@ -0,0 +1,20 @@
+public class Mergesort {
+
+    public Arraylist<Integer> a, b;
+
+    public Mergesort( Arraylist<Integer> c, Arraylist<Integer> d ) {
+	a = c;
+	b = d;
+    }
+
+    public Arraylist<Integer> sort() {
+	
+    }
+
+    public static void main( String[] args ) {
+	Mergesort M = new Mergesort();
+	Arraylist<Integer> a = M.sort();
+	System.out.println(a);
+    }
+
+}
