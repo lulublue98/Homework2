1c77a863a5d701ecebd78755a5cda9bd09183b11
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-03-05 09:27:06 -0500

    select

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -1,3 +1,25 @@
 public class Selection {
+    
+    public int[] list;
+    
+    public Selection( int[] l ) {
+	list = l;
+    }
+
+    public int select( int k, int[] l ) {
+	int[] temp = new int[l.length];
+	int pivot = l[0];
+	for (int i=0;i<l.length;i++) {
+	    if ( l[i] > pivot ) {
+
+	    } else {
+
+	    }
+	}
+	if ( temp[k] != pivot ) {
+	    select( int k, temp );
+	}
+	return temp[k];
+    }
 
 }
