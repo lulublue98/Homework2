894a12a62832993584c8fcef51870f3f96a27f8e
lulublue98 <lulu.jiyun.cho@gmail.com>
2015-03-06 06:58:24 -0500

    good

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Selection {
     
     public int[] list;
@@ -9,17 +11,32 @@ public class Selection {
     public int select( int k, int[] l ) {
 	int[] temp = new int[l.length];
 	int pivot = l[0];
+	int endnum = l.length - 1;
+	int frontnum = 0;
 	for (int i=0;i<l.length;i++) {
 	    if ( l[i] > pivot ) {
-
+		temp[endnum] = l[i];
+		endnum = endnum - 1;
 	    } else {
-
+		temp[frontnum] = l[i];
+		frontnum = frontnum + 1;
 	    }
 	}
 	if ( temp[k] != pivot ) {
-	    select( int k, temp );
+	    int[] temp2 = new int[Arrays.asList(temp).indexOf(pivot)];
+	    for (int j=0;j<=Arrays.asList(temp).indexOf(pivot);j++) {
+		temp2[j] = temp[j];
+	    }
+	    select( k, temp2 );
 	}
 	return temp[k];
+    }
+
+    public static void main(String[] args) {
+	int[] c = { 1, 0, 9, 8, 5, 
+		    3, 4, 7, 2, 6 };
+	Selection s = new Selection(c);
+	System.out.println(s.select(3,c));
     }
 
 }
