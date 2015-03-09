722200c8a6d74206e6b8ba57ea46454c62cef0bc
lulublue98 <lulu.jiyun.cho@gmail.com>
2015-03-09 00:33:09 -0400

    hw

--- 05-selection/Selection.java
+++ 05-selection/Selection.java
@@ -1,42 +1,32 @@
 import java.util.*;
+import java.io.*;
 
 public class Selection {
-    
-    public int[] list;
-    
-    public Selection( int[] l ) {
-	list = l;
-    }
 
-    public int select( int k, int[] l ) {
-	int[] temp = new int[l.length];
-	int pivot = l[0];
-	int endnum = l.length - 1;
-	int frontnum = 0;
-	for (int i=0;i<l.length;i++) {
-	    if ( l[i] > pivot ) {
-		temp[endnum] = l[i];
-		endnum = endnum - 1;
+    public int select( int k, int[] A, int L, int H ) {
+	int pivot = A[L];
+	int[] temp == new int[A.length-1];
+	temp[H] = pivot;
+	for (int i=0;i<A.length;i++) {
+	    if ( A[i] > pivot ) {
+		temp[H] = A[i];
+		H = H - 1;
 	    } else {
-		temp[frontnum] = l[i];
-		frontnum = frontnum + 1;
+		temp[L] = A[i];
+		L = L + 1;
 	    }
 	}
-	if ( temp[k] != pivot ) {
-	    int[] temp2 = new int[Arrays.asList(temp).indexOf(pivot)];
-	    for (int j=0;j<=Arrays.asList(temp).indexOf(pivot);j++) {
-		temp2[j] = temp[j];
-	    }
-	    select( k, temp2 );
+	if ( k != ) {
+	    select( k, temp, 0, pivot );
 	}
 	return temp[k];
     }
 
     public static void main(String[] args) {
-	int[] c = { 1, 0, 9, 8, 5, 
+	int[] a = { 1, 0, 9, 8, 5, 
 		    3, 4, 7, 2, 6 };
-	Selection s = new Selection(c);
-	System.out.println(s.select(3,c));
+	Selection s = new Selection();
+	System.out.println(s.select(3,a, 0, a.length - 1));
     }
 
 }
