e6ef2507265f4e7e25a523314ab3e1a4ca8dfb31
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-03-12 09:23:31 -0400

    Linking up

--- 07-LinkedList/Driver.java
+++ 07-LinkedList/Driver.java
@@ -3,7 +3,11 @@ public class Driver {
     public static void main(String[] args) {
 
 	LinkedList L = new LinkedList();
-	L.add("hello");
+	for (int i=1;i<11;i++) {
+	    String s = "";
+	    L.add(s + i);
+	}
+	System.out.println(L);
 
     }
 
--- 07-LinkedList/LinkedList.java
+++ 07-LinkedList/LinkedList.java
@@ -2,7 +2,7 @@ public class LinkedList {
 
     private Node n = null;
     
-    public void add(Node s) {
+    public void add(String s) {
 	Node tmp = new Node(s);
 	tmp.setNext(n);
 	n = tmp;
@@ -11,8 +11,8 @@ public class LinkedList {
     public String toString() {
 	String s = "";
 	Node tmp;
-	for (tmp=temp;tmp!=null;tmp=tmp.getNext()) {
-	    s = s + tmp + "-->";
+	for (tmp=n;tmp!=null;tmp=tmp.getNext()) {
+	    s = s + tmp + " --> ";
 	}
 	s = s + "null";
 	return s;
