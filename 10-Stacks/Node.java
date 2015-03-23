5dd26f828a8012ff36b203599444dabe7731c883
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-03-23 09:23:07 -0400

    L

--- 10-Stacks/Node.java
+++ 10-Stacks/Node.java
@@ -2,18 +2,27 @@ public class Node<E> {
 
     private E data;
     private Node<E> next;
+    private Node<E> prev;
 
     public Node() {
 	data = null;
 	next = null;
+	prev = null;
     }
 
-    public Node(E s) {
+    public Node( E s ) {
 	data = s;
 	next = null;
+	prev = null;
     }
 
-    public void setData(E s) { 
+    public Node( Node p, Node n,  E s ) {
+	data = s;
+	next = n;
+	prev = p;
+    }
+
+    public void setData( E s ) { 
 	data = s;
     }
 
@@ -21,7 +30,7 @@ public class Node<E> {
 	return data;
     }
 
-    public void setNext(Node<E> n) {
+    public void setNext( Node<E> n ) {
 	next = n;
     }
 
--- 10-Stacks/myStack.java
+++ 10-Stacks/myStack.java
@@ -1,23 +1,43 @@
 public class myStack<E> {
 
-    // You decide how the node(s) are declared
+    private Node top;
+    private Node bottom;
 
-    // make whatever constructor(s) you need
+    public myStack() {
+	bottom = null;
+	top = null;
+    }
+
+    public myStack( Node b ) {
+	bottom = b;
+	top = b;
+    }
 
     public void push( E data ) {
-        // add something to the top
+	Node n = new Node(top, null, data);
+        top.setNext(n);
+	top = n;
     }
 
     public E pop() {
-        // remove and return the top item from the stack
+	if (bottom=null){
+	    return top;
+	}
+        Node tmp = top;
+	top.getPrev().setNext(null);
+	return tmp;
     }
 
     public boolean empty() {
-
+	if (bottom=null){
+	    return true;
+	}
+	else 
+	    return false;
     }
 
     public E top() {
-        // return the top item from the stack
+	return top.getData();
     }
 
 }
