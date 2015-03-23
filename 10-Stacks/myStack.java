efb489718fdeff158ed55d0b960ed5bd84372828
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-03-23 09:12:41 -0400

    L

--- 10-Stacks/myStack.java
+++ 10-Stacks/myStack.java
@@ -1,21 +1,23 @@
-public class myStack<E>{
+public class myStack<E> {
+
     // You decide how the node(s) are declared
 
     // make whatever constructor(s) you need
 
-    public void push(E data){
+    public void push( E data ) {
         // add something to the top
     }
 
-    public E pop(){
+    public E pop() {
         // remove and return the top item from the stack
     }
 
-    public boolean empty(){
+    public boolean empty() {
 
     }
 
     public E top() {
         // return the top item from the stack
     }
+
 }
