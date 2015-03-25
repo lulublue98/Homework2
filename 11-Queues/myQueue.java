ee316bd1902ddfec2f2f061abf7a2ee43b8dadea
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-03-25 09:24:11 -0400

    queue up

--- 11-Queues/myQueue.java
+++ 11-Queues/myQueue.java
@@ -1,26 +1,54 @@
-public class myQueue<E>{
+public class myQueue<E> {
 
     private Node<E> front;
     private Node<E> back;
 
     public myQueue() {
-
+	front = null;
+	back = null;
+	front.setNext(back);
+	back.setPrev(front);
     }
 
     public void enqueue(E data){
-        // add something to the tail/last
+        if ( empty() ) {
+	    front = new Node<E>(data);
+	    front.setNext(back);
+	    back.setPrev(front);
+	} else {
+	    Node<E> tmp = new Node<E>(data);
+	    back.setNext(tmp);
+	    tmp.setPrev(back);
+	    back = tmp;
+	}
     }
 
-    public E dequeue(){
-        // remove and return the head/front item from the stack
+    public E dequeue() { 				
+	if ( empty() ) {
+	    return null;
+	}
+        E tmp = front.getData();
+	front = front.getNext();
+	return tmp;
     }
 
-    public boolean empty(){
-	
+    public boolean empty() { 
+	return front == null;
     }
 
     public E head() {
-        // return the first item in the queue
+	return front.getData();
+    }
+
+    public String toString() {
+	String s = "";
+	Node<E> tmp = front;
+	while ( tmp != null ) {
+	    s = s + tmp.getData() + " <-- ";
+	    tmp = tmp.getNext();
+	}
+	s = s + "null";
+	return s;
     }
 
 }
