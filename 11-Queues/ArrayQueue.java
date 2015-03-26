bf0ffff3b247da0bb75b9d481294a441ce7d3ed6
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-03-26 09:26:34 -0400

    Queue Stuff

--- 11-Queues/ArrayQueue.java
+++ 11-Queues/ArrayQueue.java
@@ -1,5 +1,29 @@
 public class ArrayQueue {
 
-    
+    private int[] queue;
+    private int index;
+
+    public ArrayQueue() {
+	queue = new int[10];
+	index = 0;
+    }
+
+    public void grow() {
+	int[] tmp = new int[queue.length+10];
+	for (int i=0;i<queue.length;i++) {
+	    tmp[i] = queue[i];
+	}
+	queue = tmp;
+	index = index + 1;
+    }
+
+    public void enqueue( int data ) {
+	queue[index] = data;
+	index = index + 1;
+    }
+
+    public boolean full() {
+
+    }
 
 }
--- 11-Queues/Driver2.java
+++ 11-Queues/Driver2.java
@@ -2,7 +2,7 @@ public class Driver {
 
     public static void main( String[] args ) {
 
-	
+	ArrayQueue A = new ArrayQueue();
 
     }
 
