dba332580499b29ec476f3a830e6d273c1b4ba2c
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-04-13 09:17:33 -0400

    lulu

---
+++ 12-VacationHW/cors.java
@@ -0,0 +1,19 @@
+public class cors {
+
+    private int x;
+    private int y;
+
+    public cors( int xcor, int ycor ) {
+	x = xcor;
+	y = ycor;
+    }
+
+    public int getX() {
+	return x;
+    }
+
+    public int getY() {
+	return y;
+    }
+
+}
