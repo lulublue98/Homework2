a02dae210be8b4f952d1ad6183232b80b5da2ec0
Lulu Cho <lulu.jiyun.cho@gmail.com>
2015-05-11 09:25:16 -0400

    java

--- 14-trees/BST.java
+++ 14-trees/BST.java
@@ -69,10 +69,30 @@ public class BST {
 	}
     }
     
+    public int numNodes( Node t ) {
+	if ( t == null ) {
+	    return 0;
+	} else {
+	    return 1 + numNodes(t.getLeft()) + numNodes(t.getRight());
+	}
+    }
+    public int size() {
+	return numNodes( root );
+    }
+
     public String toString() {
 	return traverse(root);
     }
     
+    public int maxValue() {
+	Node tmp = root;
+	while ( tmp.getRight() != null ) {
+	    tmp = tmp.getRight();
+	}
+	return tmp.getData();
+    }
+
+    // this method is broken
     public void remove( int i ) {
 	Node t1 = search( i, root );
 	Node t2 = t1.getParent();
@@ -110,6 +130,25 @@ public class BST {
 	}
     }
 
+    public int getHeight( Node t ) {
+	if ( t == null ) {
+	    return 0;
+	} else {
+	    return 1 + Math.max(getHeight(t.getLeft()),getHeight(t.getRight()));
+	}
+    } 
+    /*
+    public int height() {
+	return getHeight(root);
+    }
+    public int longest() {
+
+    }
+    
+    public int splitDupes() {
+
+    }
+    */
     public static void main( String[] args ) {
 	Node N = new Node(25);
 	BST tree = new BST(N);
@@ -119,8 +158,12 @@ public class BST {
 	    tree.insert(x);
 	}
 	System.out.println(tree);
-	tree.remove(30);
-	System.out.println(tree);
+	//System.out.println(tree.search(30));
+	//tree.remove(30);
+	//System.out.println(tree);
+	System.out.println(tree.maxValue());
+	System.out.println(tree.size());
+	System.out.println(tree.height());
     }
 
 }
